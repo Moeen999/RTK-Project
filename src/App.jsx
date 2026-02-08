9f74e50fcc9e8556162dd2984fb38da3abfbd638
Muhammad Moeen <mooenmalik374@gmail.com>
2026-02-08 16:06:46 +0500

    Added Unsplash photo fetching via getPhotos, updated App.jsx with fetch button, introduced new result/search/collection components and Redux slices, and updated .gitignore to include .env.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { getPhotos } from "./api/mediaApi";
 
 const App = () => {
   return (
     <div>
       RTK Project
+      <button
+        className="border m-5 bg-orange-600 p-3"
+        onClick={() => getPhotos("lion")}
+      >
+        Get Photos Data
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
