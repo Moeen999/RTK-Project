3db026d1840b8a9399ffe19780640dce49ce0695
Muhammad Moeen <mooenmalik374@gmail.com>
2026-02-10 01:02:43 +0500

    Refactor App component to include a header for improved layout and structure.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import Tabs from "./components/Tabs";
 
 const App = () => {
   return (
-    <div className="h-screen w-full bg-gray-950 text-white">
-      <Searchbar/>
-      <Tabs/>
-      <ResultGrid/>
-    </div>
+    <>
+      <h1 className="bg-gray-900 text-white flex justify-center items-center py-3 sm:text-lg sm:py-2 md:text-3xl font-bold">
+        RTK API Practice App
+      </h1>
+      <div className="h-screen w-full bg-gray-950 text-white">
+        <Searchbar />
+        <Tabs />
+        <ResultGrid />
+      </div>
+    </>
   );
 };
 
